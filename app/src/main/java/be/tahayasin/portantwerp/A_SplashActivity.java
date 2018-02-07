a6ef34a93e27b8870fe7f6108793a98ff7444323
Savran Taha <savrantaha@hotmail.com>
2018-02-07 15:04:06 +0100

    Created the project folder

---
+++ app/src/main/java/be/tahayasin/portantwerp/A_SplashActivity.java
@@ -0,0 +1,13 @@
+package be.tahayasin.portantwerp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A_SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_activity_splash);
+    }
+}
