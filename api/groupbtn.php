cdfdc6bd16b0ce05dead0b8db16ff71bbc3bb2ad
ivo08 <ivo.08.santos@noreply.com>
2018-05-10 18:01:10 +0200

    Menu actions

--- api/groupbtn.php
+++ api/groupbtn.php
@@ -3,31 +3,29 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
   <body>
-
+    <?php
+    $errormessage = '';
+    echo $errormessage;
+    ?>
 <div class="dropdown">
-<button onclick="myFunction()" class="btn btn_blue">Dropdown</button>
+<button onclick="myFunction()" class="btn btn_blue">Menu</button>
   <div id="myDropdown" class="dropdown-content">
-    <a href="api/action.php">START</a>
-    <a href="">About</a>
-    <a href="">Contact</a>
+
+    <!-- 0=stop 1=start 2=pause -->
+
+    <?php if ($status==0){?>
+          <a href="api/startaction.php">START</a>
+    <?php }
+          if ($status!=0){?>
+          <a href="api/stopaction.php">STOP</a>
+    <?php }
+          if ($status==1){?>
+          <a href="api/pauseaction.php">PAUSE</a>
+    <?php }
+          if ($status==2){?>
+          <a href="api/startaction.php">RESUME</a>
+      <?php } ?>
   </div>
 </div>
-
-  <!--  <?php switch ($status) {
-      case '0':?>
-          <button class="btn btn_blue" onclick="btnstart()"> <?php print($currentbtn) ?></button>
-        <?php  break;
-      case '1': ?>
-          <button class="btn btn_blue" onclick=""><?php print($currentbtn) ?></button>
-        <?php  break;
-      case '2': ?>
-          <button class="btn btn_blue" onclick=""><?php print($currentbtn) ?></button>
-        <?php  break;
-      default:
-
-        break;
-    } ?> -->
-
-
   </body>
 </html>
---
+++ api/stopaction.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../db.php';
+
+$sql = "SELECT Name, status FROM groups WHERE Name='Port Team'";
+$result = mysqli_query($conn, $sql);
+while ($row = $result->fetch_assoc()) {
+    $nameteam = $row['Name'];
+    $currentstatus = $row['status'];
+}
+if ($currentstatus!=0) {
+  $sqlupdate = "UPDATE groups SET status=0 WHERE Name='$nameteam'";
+  if($conn->query($sqlupdate) === TRUE) {
+      echo 'Sucessly updated';
+      header('location: ../index.php#!/groups');
+  } else {
+      echo  $conn->error;
+  }
+} else{
+      echo 'Game already running';
+}
+?>
