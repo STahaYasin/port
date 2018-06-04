8c44fe5529d2b5df8b5482d40bd31b93699f3197
Savran Taha <savrantaha@hotmail.com>
2018-06-04 10:50:57 +0200

    Added join team

--- app/jointeam.php
+++ app/jointeam.php
@@ -7,6 +7,7 @@ $res = new Result();
 
 $groupid = $_POST["groupid"];
 $teamid = $_POST["teamid"];
+$userid = $_POST["userid"];
 
 if($groupid == null || $groupid == ""){
     $res->success = false;
@@ -23,7 +24,7 @@ if($teamid == null || $teamid == ""){
     die(json_encode($res));
 }
 
-$sql_get_group = "SELECT * FROM `teams` WHERE `T_ID` = '$groupid' AND `G_ID` = '$teamid'";
+$sql_get_group = "SELECT * FROM `teams` WHERE `T_ID` = '$teamid' AND `G_ID` = '$groupid'";
 $res = new Result();
 $res->success = false;
 $res->message = "Server error";
@@ -41,16 +42,55 @@ if($group == null){
 
 $sqlcount = "SELECT COUNT(*) as count FROM users WHERE `T_ID`= '$teamid'";
 
+$res = new Result();
+$res->success = false;
+$res->message = "Server error";
+$res->data = null;
+$sql_res_count = mysqli_query($conn, $sqlcount) or die (json_encode($res));
+$count = mysqli_fetch_assoc($sql_res_count);
 
-/////////////////////// Check for count, then update the teamid of the user
+if($count == null){
+    $res = new Result();
+    $res->success = false;
+    $res->message = "Team does not excist!";
+    $res->data = null;
+    die(json_encode($res));
+}
 
-$sql_insert_user = "INSERT INTO `users` (`U_ID`, `T_ID`, `Name`, `x`, `y`, `level`) VALUES (NULL, NULL, '$user_name', '0', '0', '0');";
-if ($conn->query($sql_insert_user) === TRUE) {
+if((int)$count["count"] >= (int)$group["max_students"]){
+    $res = new Result();
+    $res->success = false;
+    $res->message = "Team already filled up";
+    $res->data = (int)$count["count"];
+    die(json_encode($res));
+}
+
+$sqluser = "SELECT * FROM users WHERE U_ID = $userid";
+
+$res = new Result();
+$res->success = false;
+$res->message = "Server error";
+$res->data = null;
+$sql_res_user = mysqli_query($conn, $sqluser) or die (json_encode($res));
+$user = mysqli_fetch_assoc($sql_res_user);
+
+if($user == null){
+    $res = new Result();
+    $res->success = false;
+    $res->message = "User does not excist!";
+    $res->data = null;
+    die(json_encode($res));
+}
+
+
+$sql_update = "UPDATE `users` SET `T_ID` = $teamid WHERE `users`.`U_ID` = $userid";
+
+if ($conn->query($sql_update) === TRUE) {
     $res = new Result();
     $last_id = $conn->insert_id;
     $res->success = true;
-    $res->message = "User created successfully";
-    $res->data = $last_id;
+    $res->message = "User updated successfully";
+    $res->data = (int)$teamid;
     die(json_encode($res));
 } else {
     $res = new Result();
